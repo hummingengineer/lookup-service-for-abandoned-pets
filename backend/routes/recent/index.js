efc72eef331f73d3bf22b9ea7e04127d1ca8829a
hummingengineer <hummingengineer@gmail.com>
2020-01-16 01:53:08 +0900

    Recent Dog Data Request

--- backend/routes/recent/index.js
+++ backend/routes/recent/index.js
@@ -1,8 +1,17 @@
 const Router = require('@koa/router')
 const router = new Router()
+const axios = require('axios')
 
-router.get('/', ctx => {
-  ctx.body = 'Router Success'
+// 비구조화 할당을 통하여 process.env 내부 값에 대한 레퍼런스 만들기
+require('dotenv').config()
+const { SK } = process.env
+
+router.get('/dog', async ctx => {
+  const { data } = await axios.get(`http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?upkind=417000&pageNo=1&ServiceKey=${SK}`)
+  ctx.body = {
+    recentList: data.response.body.items.item,
+    totalCount: data.response.body.totalCount
+  }
 })
 
 module.exports = router
