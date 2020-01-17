d51a87322de856709f3551c7e9409ac68ba86f69
hummingengineer <hummingengineer@gmail.com>
2020-01-18 04:48:17 +0900

    Setting card grid layout

--- frontend/src/components/ItemList.vue
+++ frontend/src/components/ItemList.vue
@@ -2,18 +2,20 @@
   <div>
     <v-row v-for="(item, i) in recentList" :key="i">
       <v-col>
-        <v-card hover>
-          <v-avatar size="125" tile>
+        <v-card class="d-sm-flex d-md-flex d-lg-flex d-xl-flex text-center text-sm-left text-md-left text-lg-left text-xl-left" hover>
+          <v-avatar class="ma-2" size="160" tile>
             <v-img :src="item.filename" ></v-img>
           </v-avatar>
-          <v-card-title>유기번호 {{ item.desertionNo }}</v-card-title>
-          <v-card-subtitle>
-            발견장소: {{ item.happenPlace }}<br />
-            품종: {{ item.kindCd }}<br />
-            색상: {{ item.colorCd }}<br />
-            성별: {{ item.sexCd === 'M' ? '수컷' : '암컷' }}<br />
-            특징: {{ item.specialMark }}
-          </v-card-subtitle>
+          <div>
+            <v-card-title class="justify-center">유기번호 {{ item.desertionNo }}</v-card-title>
+            <v-card-subtitle>
+              발견장소: {{ item.happenPlace }}<br/>
+              품종: {{ item.kindCd }}<br/>
+              색상: {{ item.colorCd }}<br/>
+              성별: {{ item.sexCd === 'M' ? '수컷' : '암컷' }}<br/>
+              특징: {{ item.specialMark }}
+            </v-card-subtitle>
+          </div>
         </v-card>
       </v-col>
     </v-row>
