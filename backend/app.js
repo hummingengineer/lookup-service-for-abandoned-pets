432108e0a276a50f30ddc1b30e9f4904669ff1b0
hummingengineer <hummingengineer@gmail.com>
2020-01-18 03:51:55 +0900

    Add comment

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const serve = require('koa-static')
 app.use(serve(require('path').join(__dirname, '../', 'frontend', 'dist')))
 
 // 라우터 설정
-router.use('/recent', require('./routes/recent/index').routes())
+router.use('/recent', require('./routes/recent/index').routes())  // './routes/recent/index' 라우트를 /recent 경로 하위 라우트로 설정
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
