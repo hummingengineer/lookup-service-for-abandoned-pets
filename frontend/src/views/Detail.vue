f74930dd1569286f893c43dcd5a16255777fbb39
hummingengineer <hummingengineer@gmail.com>
2020-01-19 23:39:43 +0900

    Add image Detail.vue

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -1,10 +1,21 @@
 <template>
   <v-container>
+
     <v-row>
       <v-col>
-        detail
+        <v-card max-width="400" class="mx-auto">
+          <v-img :src="this.$route.params.info.popfile" :contain="fullImageSwitch" max-height="435">
+            <v-row>
+              <v-spacer/>
+              <v-card-title>
+                <v-switch v-model="fullImageSwitch"/>
+              </v-card-title>
+            </v-row>
+          </v-img>
+        </v-card>
       </v-col>
     </v-row>
+
   </v-container>
 </template>
 
