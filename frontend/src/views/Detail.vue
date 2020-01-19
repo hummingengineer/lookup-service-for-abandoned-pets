dc099981fa59635ec1fdad8023a2d8b2227ca5e3
hummingengineer <hummingengineer@gmail.com>
2020-01-19 23:45:40 +0900

    Add happenDt

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -54,6 +54,18 @@
 
             <v-divider inset/>
 
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon color="indigo">mdi-calendar</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ this.$route.params.info.happenDt.toString().replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3') }}</v-list-item-title>
+                <v-list-item-subtitle>발견 날짜</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-divider inset/>
+
           </v-list>
         </v-card>
       </v-col>
