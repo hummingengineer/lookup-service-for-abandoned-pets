80ed61e5dd89a6a0099cc019c526c32853a897b2
hummingengineer <hummingengineer@gmail.com>
2020-01-21 18:35:07 +0900

    Add beginDate in Search.vue

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -2,7 +2,16 @@
   <v-container>
     <v-row>
       <v-col>
-        Search
+        <v-dialog ref="beginDialog" v-model="beginModal" :return-value.sync="beginDate" persistent width="290px">
+          <template v-slot:activator="{ on }">
+            <v-text-field v-model="beginDate" label="검색 시작일" prepend-icon="mdi-calendar" readonly v-on="on"/>
+          </template>
+          <v-date-picker v-model="beginDate" scrollable locale="ko-KR">
+            <v-spacer/>
+            <v-btn text color="primary" @click="beginModal = false">취소</v-btn>
+            <v-btn text color="primary" @click="$refs.beginDialog.save(beginDate)">확인</v-btn>
+          </v-date-picker>
+        </v-dialog>
       </v-col>
     </v-row>
   </v-container>
