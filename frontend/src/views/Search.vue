4e51a004c5a47d7424c855c1cf630bf0add7e2d8
hummingengineer <hummingengineer@gmail.com>
2020-01-21 18:38:52 +0900

    Add endDate

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+
     <v-row>
       <v-col>
         <v-dialog ref="beginDialog" v-model="beginModal" :return-value.sync="beginDate" persistent width="290px">
@@ -13,7 +14,21 @@
           </v-date-picker>
         </v-dialog>
       </v-col>
+
+      <v-col>
+        <v-dialog ref="endDialog" v-model="endModal" :return-value.sync="endDate" persistent width="290px">
+          <template v-slot:activator="{ on }">
+            <v-text-field v-model="endDate" label="검색 종료일" prepend-icon="mdi-calendar" readonly v-on="on"/>
+          </template>
+          <v-date-picker v-model="endDate" scrollable locale="ko-KR">
+            <v-spacer/>
+            <v-btn text color="primary" @click="endModal = false">취소</v-btn>
+            <v-btn text color="primary" @click="$refs.endDialog.save(endDate)">확인</v-btn>
+          </v-date-picker>
+        </v-dialog>
+      </v-col>
     </v-row>
+
   </v-container>
 </template>
 
