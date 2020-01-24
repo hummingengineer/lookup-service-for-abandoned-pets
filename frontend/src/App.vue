e2f5ed85212864b34ecb70527b4e656201798da4
hummingengineer <hummingengineer@gmail.com>
2020-01-25 02:13:30 +0900

    Add Developer's HomePage Menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,9 +23,18 @@
       <v-btn icon>
         <v-icon>mdi-filter</v-icon>
       </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>개발자 홈페이지</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
